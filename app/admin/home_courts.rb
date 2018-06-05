19cdff67fb76ee89d7c510408a28308eebcf8d75
Raghu Betina <raghu@firstdraft.com>
2018-06-05 21:31:40 +0000

    Whitelist columns in HomeCourt for ActiveAdmin

--- app/admin/home_courts.rb
+++ app/admin/home_courts.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register HomeCourt do
+
+ permit_params :teams_id, :location
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
